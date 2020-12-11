d9711c177d12fe9e14ff55a9fd5d084c42c7d0fa
Philipp-Alexander Jansen <philipp-alexander.jansen@senacor.com>
2020-12-11 17:50:36 +0100

    refactoring day 11

--- src/11/seating-system.js
+++ src/11/seating-system.js
@@ -1,136 +1,100 @@
-const SEAT_OCCUPIED = 'OCCUPIED'
-const SEAT_EMPTY = 'EMPTY'
-const DIRECTIONS = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+const { inRange, cloneDeep } = require('lodash/fp')
 
-const toKey = (rowIndex, colIndex) => `${rowIndex}-${colIndex}`
-const fromKey = (key) => key.split('-').map((index) => parseInt(index))
+const OCCUPIED = '#'
+const EMPTY = 'L'
+const FLOOR = '.'
 
-// finding and saving seats with coordinates for faster access
-const findSeats = (input) => {
-  const height = input.length
-  const width = input[0].length
+const findSeatPositions = (seatGrid) => {
+  const seatPositions = []
 
-  const seats = {}
-
-  for (let rowIndex = 0; rowIndex < height; rowIndex++) {
-    for (let colIndex = 0; colIndex < width; colIndex++) {
-      if (input[rowIndex][colIndex] === 'L') {
-        // while it might be nicer to store it directly as a boolean, to handle
-        // differences between undefined and empty vs. occupied a string is better
-        seats[toKey(rowIndex, colIndex)] = SEAT_EMPTY
+  seatGrid.forEach((row, rowIdx) => {
+    row.forEach((cell, colIdx) => {
+      if (cell !== FLOOR) {
+        seatPositions.push([rowIdx, colIdx])
       }
+    })
+  })
+
+  return seatPositions
+}
+
+const countNeigbours = (seatGrid, row, col, { expandSearch } = {}) => {
+  const inHeightRange = inRange(0)(seatGrid.length)
+  const inWidthRange = inRange(0)(seatGrid[0].length)
+  const surrouding = [-1, 0, 1]
+
+  let neighbours = 0
+
+  surrouding.forEach((deltaRow) => {
+    surrouding.forEach((deltaCol) => {
+      if (deltaRow === 0 && deltaCol === 0) {
+        return
+      }
+
+      let newRow = row + deltaRow
+      let newCol = col + deltaCol
+      while (inHeightRange(newRow) && inWidthRange(newCol)) {
+        if (seatGrid[newRow][newCol] !== FLOOR) {
+          if (seatGrid[newRow][newCol] === OCCUPIED) {
+            neighbours++
+          }
+
+          return
+        }
+
+        if (!expandSearch) {
+          return
+        }
+
+        newRow += deltaRow
+        newCol += deltaCol
+      }
+    })
+  })
+
+  return neighbours
+}
+
+const calculateOccupiedSeats = (seatGrid, { expandNeighbourSearch, maxNeighboursThreshold }) => {
+  const seatPositions = findSeatPositions(seatGrid)
+  let currentSeatGrid = seatGrid
+
+  while (true) {
+    const newSeats = cloneDeep(currentSeatGrid)
+
+    let hasChange = false
+
+    seatPositions.forEach(([row, col]) => {
+      const neighbours = countNeigbours(currentSeatGrid, row, col, { expandSearch: expandNeighbourSearch })
+      if (currentSeatGrid[row][col] === EMPTY) {
+        if (neighbours === 0) {
+          hasChange = true
+          newSeats[row][col] = OCCUPIED
+        }
+      } else { // currentSeat === OCCUPIED
+        if (neighbours >= maxNeighboursThreshold) {
+          hasChange = true
+          newSeats[row][col] = EMPTY
+        }
+      }
+    })
+
+    currentSeatGrid = newSeats
+
+    if (!hasChange) {
+      break
     }
   }
 
-  return seats
-}
-
-const countOccupiedSeats = (seats) =>
-  Object.values(seats)
-    .filter((seat) => seat === SEAT_OCCUPIED)
+  return seatPositions
+    .map(([row, col]) => currentSeatGrid[row][col])
+    .filter((seat) => seat === OCCUPIED)
     .length
-
-const part1 = (input) => {
-  let seats = findSeats(input)
-
-  const countNeighbours = (sourceRowIndex, sourceColumnIndex) => {
-    let neighbourCount = 0
-
-    for (let rowIndex = sourceRowIndex - 1; rowIndex <= sourceRowIndex + 1; rowIndex++) {
-      for (let colIndex = sourceColumnIndex - 1; colIndex <= sourceColumnIndex + 1; colIndex++) {
-        if (rowIndex === sourceRowIndex && colIndex === sourceColumnIndex) {
-          continue
-        }
-
-        const neighbour = seats[toKey(rowIndex, colIndex)]
-        if (neighbour && neighbour === SEAT_OCCUPIED) {
-          neighbourCount++
-        }
-      }
-    }
-
-    return neighbourCount
-  }
-
-  let somethingChanged
-  do {
-    somethingChanged = false
-
-    const nextSeatsCyle = {}
-    Object.entries(seats).forEach(([key, value]) => {
-      const [rowIndex, colIndex] = fromKey(key)
-      const neighbours = countNeighbours(rowIndex, colIndex)
-
-      if (seats[key] === SEAT_EMPTY && neighbours === 0) {
-        nextSeatsCyle[key] = SEAT_OCCUPIED
-        somethingChanged = true
-      } else if (seats[key] === SEAT_OCCUPIED && neighbours >= 4) {
-        nextSeatsCyle[key] = SEAT_EMPTY
-        somethingChanged = true
-      } else {
-        nextSeatsCyle[key] = seats[key]
-      }
-    })
-
-    seats = nextSeatsCyle
-  } while (somethingChanged)
-
-  return countOccupiedSeats(seats)
 }
 
-const part2 = (input) => {
-  const height = input.length
-  const width = input[0].length
-  let seats = findSeats(input)
+const part1 = (seatGrid) => calculateOccupiedSeats(seatGrid, { expandNeighbourSearch: false, maxNeighboursThreshold: 4 })
 
-  const countNeighbours = (sourceRowIndex, sourceColumnIndex) => {
-    let neighbourCount = 0
-
-    DIRECTIONS.forEach(([rowDelta, colDelta]) => {
-      let i = 1
-
-      const rowIndex = () => sourceRowIndex + rowDelta * i
-      const colIndex = () => sourceColumnIndex + colDelta * i
-      const inBounds = () => rowIndex() >= 0 && rowIndex() < height && colIndex() >= 0 && colIndex() < width
-
-      // expand until a seat is found or we are out of bounds
-      while (inBounds() && !seats[toKey(rowIndex(), colIndex())]) {
-        i++
-      }
-
-      if (inBounds() && seats[toKey(rowIndex(), colIndex())] === SEAT_OCCUPIED) {
-        neighbourCount++
-      }
-    })
-
-    return neighbourCount
-  }
-
-  let somethingChanged
-  do {
-    somethingChanged = false
-
-    const nextSeatsCyle = {}
-    Object.entries(seats).forEach(([key, value]) => {
-      const [rowIndex, colIndex] = fromKey(key)
-      const neighbours = countNeighbours(rowIndex, colIndex)
-
-      if (seats[key] === SEAT_EMPTY && neighbours === 0) {
-        nextSeatsCyle[key] = SEAT_OCCUPIED
-        somethingChanged = true
-      } else if (seats[key] === SEAT_OCCUPIED && neighbours >= 5) {
-        nextSeatsCyle[key] = SEAT_EMPTY
-        somethingChanged = true
-      } else {
-        nextSeatsCyle[key] = seats[key]
-      }
-    })
-
-    seats = nextSeatsCyle
-  } while (somethingChanged)
-
-  return countOccupiedSeats(seats)
-}
+const part2 = (seatGrid) => calculateOccupiedSeats(seatGrid, { expandNeighbourSearch: true, maxNeighboursThreshold: 5 })
 
 module.exports = {
   part1,
